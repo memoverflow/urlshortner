5432cf0fe8ccfd25947558e5e95a81f7d7b12ea7
Lucas Ren <nichokiki@hotmail.com>
2020-07-31 01:15:11 +0800

    completed the unit test

--- src/app.js
+++ src/app.js
@@ -30,6 +30,7 @@ let dynamo = new AWS.DynamoDB.DocumentClient({
  *
  */
 exports.handler = async (event) => {
+  console.log(event)
   console.log("event")
   if (event.httpMethod) {
     switch (event.httpMethod) {
