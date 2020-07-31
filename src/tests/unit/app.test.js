3f01c2ab42752a1c18c6e8b97eef70d51ad19b67
Lucas Ren <nichokiki@hotmail.com>
2020-07-31 14:41:34 +0800

    completed the unit test

--- src/tests/unit/app.test.js
+++ src/tests/unit/app.test.js
@@ -3,82 +3,79 @@ const app = require("../../app");
 
 // This includes all tests for app.js
 describe("Test app handler", () => {
-  test('should 1', () => {
-    
-  })
 
-  // // This test invokes app.handler() and compare the result
-  // test('Short a long URL', async() => {
+  // This test invokes app.handler() and compare the result
+  test('Short a long URL', async() => {
       
-  //   // Mock the event json
-  //   const event = {
-  //     path: "/url",
-  //     httpMethod: "POST",
-  //     body: '{"url":"https://github.com/awsdocs/aws-lambda-developer-guide/tree/master/sample-apps/nodejs-apig",    "token":"2sdfdsf312312"}',
-  //   };
+    // Mock the event json
+    const event = {
+      path: "/url",
+      httpMethod: "POST",
+      body: '{"url":"https://github.com/awsdocs/aws-lambda-developer-guide/tree/master/sample-apps/nodejs-apig",    "token":"2sdfdsf312312"}',
+    };
 
-  //   // Excute the handler and got a promise result
-  //   let resp = await app.handler(event);
+    // Excute the handler and got a promise result
+    let resp = await app.handler(event);
 
-  //   // Declare a expect result
-  //   const expectResult = {
-  //     statusCode: 200,
-  //     headers: {
-  //       "Access-Control-Allow-Origin": "*",
-  //       "Access-Control-Allow-Methods": "POST, DELETE, PUT",
-  //     },
-  //     body:
-  //       '{"hashid":"aDoneEI","l_url":"https://github.com/awsdocs/aws-lambda-developer-guide/tree/master/sample-apps/nodejs-apig","s_url":"https://api.sname.be/aDoneEI","create_at":"Thu, 30 Jul 2020 06:33:17 GMT","exp_at":"Fri, 30 Jul 2021 06:33:17 GMT","token":"2sdfdsf312312"}',
-  //   };
+    // Declare a expect result
+    const expectResult = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "POST, DELETE, PUT",
+      },
+      body:
+        '{"hashid":"aDoneEI","l_url":"https://github.com/awsdocs/aws-lambda-developer-guide/tree/master/sample-apps/nodejs-apig","s_url":"https://api.sname.be/aDoneEI","create_at":"Thu, 30 Jul 2020 06:33:17 GMT","exp_at":"Fri, 30 Jul 2021 06:33:17 GMT","token":"2sdfdsf312312"}',
+    };
 
-  //   // Compare the result
-  //   expect(resp.statusCode).toEqual(expectResult.statusCode);
+    // Compare the result
+    expect(resp.statusCode).toEqual(expectResult.statusCode);
 
-  //   expect(resp.headers[0]).toEqual(expectResult.headers[0]);
-  //   expect(resp.headers[1]).toEqual(expectResult.headers[1]);
+    expect(resp.headers[0]).toEqual(expectResult.headers[0]);
+    expect(resp.headers[1]).toEqual(expectResult.headers[1]);
 
-  //   // Convert the event body to JSON object
-  //   var entity = JSON.parse(resp.body);
-  //   var expEntity = JSON.parse(expectResult.body);
+    // Convert the event body to JSON object
+    var entity = JSON.parse(resp.body);
+    var expEntity = JSON.parse(expectResult.body);
 
-  //   expect(entity.hashid).toEqual(expEntity.hashid);
-  //   expect(entity.l_url).toEqual(expEntity.l_url);
-  //   expect(entity.s_url).toEqual(expEntity.s_url);
-  //   expect(entity.token).toEqual(expEntity.token);
+    expect(entity.hashid).toEqual(expEntity.hashid);
+    expect(entity.l_url).toEqual(expEntity.l_url);
+    expect(entity.s_url).toEqual(expEntity.s_url);
+    expect(entity.token).toEqual(expEntity.token);
       
-  // });
+  });
 
 
-  // // This test invokes app.handler() and compare the result
-  // test('Delete a long URL', async() => {
+  // This test invokes app.handler() and compare the result
+  test('Delete a long URL', async() => {
       
-  //   // Mock the event json
-  //   const event = {
-  //     path: "/url/4H3BD2x",
-  //     httpMethod: "DELETE",
-  //   };
+    // Mock the event json
+    const event = {
+      path: "/url/4H3BD2x",
+      httpMethod: "DELETE",
+    };
 
-  //   // Excute the handler and got a promise result
-  //   let resp = await app.handler(event);
+    // Excute the handler and got a promise result
+    let resp = await app.handler(event);
 
-  //   console.log(resp)
+    console.log(resp)
 
-  //   // Declare a expect result
-  //   const expectResult = {
-  //     statusCode: 200,
-  //     headers: {
-  //       "Access-Control-Allow-Origin": "*",
-  //       "Access-Control-Allow-Methods": "POST, DELETE, PUT",
-  //     },
-  //     body: '"DELETED"',
-  //   };
+    // Declare a expect result
+    const expectResult = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "POST, DELETE, PUT",
+      },
+      body: '"DELETED"',
+    };
 
-  //   // Compare the result
-  //   expect(resp.statusCode).toEqual(expectResult.statusCode);
-  //   expect(resp.body).toEqual(expectResult.body);
+    // Compare the result
+    expect(resp.statusCode).toEqual(expectResult.statusCode);
+    expect(resp.body).toEqual(expectResult.body);
 
-  //   expect(resp.headers[0]).toEqual(expectResult.headers[0]);
-  //   expect(resp.headers[1]).toEqual(expectResult.headers[1]);
-  // });
+    expect(resp.headers[0]).toEqual(expectResult.headers[0]);
+    expect(resp.headers[1]).toEqual(expectResult.headers[1]);
+  });
     
 });
--- src/tests/unit/hash.test.js
+++ src/tests/unit/hash.test.js
@@ -4,82 +4,80 @@ const app = require("../../app");
 
 // This includes all tests for hash.js
 describe("Test hash handler success", () => {
+  let hashid;
 
-    test("should w", () => {});
-  // let hashid;
+  // Prepare mock data for hash handler
+  beforeAll(async() => {
+    const event = {
+      path: "/url",
+      httpMethod: "POST",
+      body:
+        '{"url":"https://github.com/awsdocs/aws-lambda-developer-guide/tree/master/sample-apps/nodejs-apig",    "token":"2sdfdsf312312"}',
+    };
+    const demoData = await app.handler(event);
+    hashid = JSON.parse(demoData.body).hashid;
+  });
 
-  // // Prepare mock data for hash handler
-  // beforeAll(async() => {
-  //   const event = {
-  //     path: "/url",
-  //     httpMethod: "POST",
-  //     body:
-  //       '{"url":"https://github.com/awsdocs/aws-lambda-developer-guide/tree/master/sample-apps/nodejs-apig",    "token":"2sdfdsf312312"}',
-  //   };
-  //   const demoData = await app.handler(event);
-  //   hashid = JSON.parse(demoData.body).hashid;
-  // });
+  // Clean up mocks 
+  afterAll(async() => { 
+    // Mock the event json
+    const event = {
+      path: "/url/"+hashid ,
+      httpMethod: "DELETE"
+    };
+    await app.handler(event);
+  }); 
 
-  // // Clean up mocks 
-  // afterAll(async() => { 
-  //   // Mock the event json
-  //   const event = {
-  //     path: "/url/"+hashid ,
-  //     httpMethod: "DELETE"
-  //   };
-  //   await app.handler(event);
-  // }); 
+  // This test invokes hash.handler() and compare the result
+  test('Redirect URL', async() => {
+    // Mock the event json
+    const event = {
+      path: "/" + hashid,
+      httpMethod: "GET"
+    };
 
-  // // This test invokes hash.handler() and compare the result
-  // test('Redirect URL', async() => {
-  //   // Mock the event json
-  //   const event = {
-  //     path: "/" + hashid,
-  //     httpMethod: "GET"
-  //   };
+    // Excute the handler and got a promise result
+    let resp = await hash.handler(event);
 
-  //   // Excute the handler and got a promise result
-  //   let resp = await hash.handler(event);
+    // Declare a expect result
+    const expectResult = {
+      statusCode: 301,
+      headers: {
+        Location: "https://github.com/awsdocs/aws-lambda-developer-guide/tree/master/sample-apps/nodejs-apig"
+      },
+      body: '""'
+    };
 
-  //   // Declare a expect result
-  //   const expectResult = {
-  //     statusCode: 301,
-  //     headers: {
-  //       Location: "https://github.com/awsdocs/aws-lambda-developer-guide/tree/master/sample-apps/nodejs-apig"
-  //     },
-  //     body: '""'
-  //   };
+    // Compare the result
+    expect(resp.statusCode).toEqual(expectResult.statusCode);
 
-  //   // Compare the result
-  //   expect(resp.statusCode).toEqual(expectResult.statusCode);
+    expect(resp.headers.Location).toEqual(expectResult.headers.Location);
 
-  //   expect(resp.headers.Location).toEqual(expectResult.headers.Location);
+  });
 
-  // });
+  // This test invokes hash.handler() and return a 400 status code
+  test('Redirect a invlid URL', async() => {
+    // Mock the event json
+    const event = {
+      path: "/12345678",
+      httpMethod: "GET"
+    };
 
-  // // This test invokes hash.handler() and return a 400 status code
-  // test('Redirect a invlid URL', async() => {
-  //   // Mock the event json
-  //   const event = {
-  //     path: "/12345678",
-  //     httpMethod: "GET"
-  //   };
+    // Excute the handler and got a promise result
+    let resp = await hash.handler(event);
 
-  //   // Excute the handler and got a promise result
-  //   let resp = await hash.handler(event);
+    console.log(resp);
 
-  //   console.log(resp);
+    //Declare a expect result
+    const expectResult = {
+      statusCode: 404,
+      body: "\"Please passa valid URL\"",
+    };
 
-  //   //Declare a expect result
-  //   const expectResult = {
-  //     statusCode: 404,
-  //     body: "\"Please passa valid URL\"",
-  //   };
+    // Compare the result
+    expect(resp.statusCode).toEqual(expectResult.statusCode);
 
-  //   // Compare the result
-  //   expect(resp.statusCode).toEqual(expectResult.statusCode);
+    expect(resp.body).toEqual(expectResult.body);
 
-  //   expect(resp.body).toEqual(expectResult.body);
-
-  // });
+  });
 });
